f6809596439523cd284e6fa073804b47f7e66dd1
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-28 17:59:16 -0500

    updated and added nav bar

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,10 +2,17 @@ import React from "react";
 import logo from "../IMG/Tender Logo WIP.png"
 
 function homepage(props) {
-  const tabs = ["Home", "About", "Login", "Sign Up","Help"];
+  const tabs = ["Home", "About", "Login", "Help"];
+
   return (
+
+
     <div className="tabs is-centered">
-         <img className="TenderLogo" src={logo} alt="logo" />
+      {/* <!-- Latest compiled and minified CSS --> */}
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
+
+      {/* <!-- Optional theme --> */}
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>
       <ul className="nav nav-tabs">
         {tabs.map((tab, i) => (
           <li
@@ -18,15 +25,19 @@ function homepage(props) {
               href={"#" + tab.toLowerCase()}
               // Whenever a tab is clicked on,
               // the current page is set through the handlePageChange props.
-              onClick= {()=>props.setcurrentPage(i)}
-         
+              onClick={() => props.setcurrentPage(i)}
+
             >
               {tab}
             </a>
           </li>
         ))}
       </ul>
+      <img className="TenderLogo" src={logo} alt="logo" />
+
+
     </div>
+
   );
 }
 
