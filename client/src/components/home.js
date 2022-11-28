6773af9965a4741a9a016815cf9f026996e71895
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-11-28 18:26:06 -0500

    cleaned up buttons

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -8,11 +8,15 @@ function homepage(props) {
 
 
     <div className="tabs is-centered">
+      
       {/* <!-- Latest compiled and minified CSS --> */}
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
 
       {/* <!-- Optional theme --> */}
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous"></link>
+
+
+
       <ul className="nav nav-tabs">
         {tabs.map((tab, i) => (
           <li
@@ -35,7 +39,10 @@ function homepage(props) {
       </ul>
       <img className="TenderLogo" src={logo} alt="logo" />
 
-
+      <button type="button" onclick="alert('You pressed the button!')" className="loginBtn">Login
+      </button>
+      <button type="button" onclick="alert('You pressed the button!')" className="signupBtn">Sign Up
+      </button>
     </div>
 
   );
