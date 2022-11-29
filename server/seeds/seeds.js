363a3803f27b624e97ad82e90526c2e4887077f3
Novo Persona <adcaldwell93@yahoo.com>
2022-11-28 19:06:46 -0500

    created profile seed data to test

--- server/seeds/seeds.js
+++ server/seeds/seeds.js
@@ -1,1 +1,15 @@
-// file for seeds.js
+const db = require('../config/connection');
+const { Parent } = require('../models');
+const parentSeeds = require('./parentData.json');
+
+db.once('open', async () => {
+    try {
+        await Parent.deleteMany({});
+        await Parent.create(parentSeeds);
+
+        console.log('all done');
+        process.exit(0);
+    } catch (err) {
+        throw err;
+    }
+    });
