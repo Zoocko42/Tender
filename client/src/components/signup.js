8c8d9f162edd44ef86b17a1b3791a53ceb7faaa8
Zachariah Schneider <zoocko@gmail.com>
2022-11-30 11:00:26 -0500

    This update adds functionality to the login button on the Home page

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SignUpContainer from "./src/SignUpContainer"
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import './index.css'
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import SignUpContainer from "./src/SignUpContainer"
+// import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+// import './index.css'
 
-const App = () => (
-  <MuiThemeProvider>
-    <SignUpContainer />
-  </MuiThemeProvider>
-);
+// const App = () => (
+//   <MuiThemeProvider>
+//     <SignUpContainer />
+//   </MuiThemeProvider>
+// );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
