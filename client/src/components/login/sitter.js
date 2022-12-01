a3c3146320e859dee3cb20bac68d0f3283241702
Zachariah Schneider <zoocko@gmail.com>
2022-11-30 21:04:46 -0500

    Added functionality for the login page for sitters; page now correctly checks username and password against server info.

--- client/src/components/login/sitter.js
+++ client/src/components/login/sitter.js
@@ -1,46 +1,85 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Link } from "react-router-dom"
+import { useMutation } from '@apollo/client';
+import {LOGIN_SITTER} from '../../utils/mutations'
 
-export default function MyForm() {
-  const [inputs, setInputs] = useState({});
+import Auth from '../../utils/auth'
+
+const Login = (props) => {
+  const [inputs, setInputs] = useState({ username:'', password:''});
+  const [login, {error, data}] = useMutation(LOGIN_SITTER)
 
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({ ...values, [name]: value }))
-  }
+    const {name, value} = event.target;
 
-  const handleSubmit = (event) => {
+    setInputs({ ...inputs, [name]: value })
+  }
+  
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    alert(inputs);
+    try {
+      const {data} = await login({
+        variables: {...inputs}
+      });
+
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setInputs({
+      username: '',
+      password: ''
+    })
   }
 
   return (
+    <div>
+    {data ? (
+      <div className='sitterLoginSuccess'>
+        <p>You're logged in! Please continue to your homepage!</p>
+        <p><Link to='/sitter/saccount'>Continue</Link></p>
+      </div>
+    ) : (
     <form onSubmit={handleSubmit}>
-      <label>username:
+      
+      <label>Username:
         <input
+          className='form-input'
           type="text"
           name="username"
+          placeholder='username'
           value={inputs.username || ""}
           onChange={handleChange}
         />
       </label>
-      <label>password:
+      <label>Password:
         <input
-          type="text"
+          className='form-input'
+          type="password"
           name="password"
+          placeholder='********'
           value={inputs.password || ""}
           onChange={handleChange}
         />
       </label>
-      <Link to="/sitter/saccount">
-        <input type="button" name="submitButton" value="Login" />
-      </Link>
-
+      <button
+        className='btn btn-block btn-primary'
+        style={{cursor: 'pointer'}}
+        type="submit"
+      >
+        Login!
+      </button>
     </form>
+    )}
+
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">
+          {error.message}
+        </div>
+      )}
+    </div>
   )
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+export default Login;
