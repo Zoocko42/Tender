956b7eedfabca1b444a00497014451c761f90cb6
Novo Persona <adcaldwell93@yahoo.com>
2022-12-01 14:06:56 -0500

    updating information

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,3 +44,66 @@ mutation Mutation($username: String!, $password: String!) {
   }
 }
 `
+export const ADD_PARENT = gql`
+mutation Mutation($username: String!, $password: String!, $email: String!, $firstName: String!, $lastName: String!, $city: String!, $state: String!) {
+    addParent(username: $username, password: $password, email: $email, firstName: $firstName, lastName: $lastName, city: $city, state: $state) {
+      token
+      user {
+        _id
+        username
+        password
+        email
+        firstName
+        lastName
+        city
+        state
+      }
+    }
+  }
+`
+
+export const ADD_SITTERREQ = gql`
+mutation Mutation($date: String!, $time: String!, $price: Int!, $city: String!, $specialRequests: String!) {
+    addSitterReq(date: $date, time: $time, price: $price, city: $city, specialRequests: $specialRequests) {
+      date
+      time
+      price
+      city
+      _id
+      submittedBy
+    }
+  }
+`
+
+export const UPDATE_SITTERREQ = gql `
+mutation Mutation($sitterReqId: ID!, $date: String, $time: String, $price: Int, $city: String, $state: String, $matched: Boolean, $specialRequests: String) {
+    updateSitterReq(sitterReqID: $sitterReqId, date: $date, time: $time, price: $price, city: $city, state: $state, matched: $matched, specialRequests: $specialRequests) {
+      specialRequests
+      state
+      city
+      price
+      time
+      date
+      _id
+      matched
+      submittedBy
+      createdAt
+    }
+  }
+`
+export const REMOVE_SITTER_REQ = gql `
+mutation Mutation($sitterReqId: ID!) {
+    removeSitterReq(sitterReqID: $sitterReqId) {
+      date
+      time
+      _id
+      price
+      city
+      submittedBy
+      state
+      specialRequests
+      matched
+      createdAt
+    }
+  }
+`
